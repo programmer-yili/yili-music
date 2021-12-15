2fc840a62cfebb9a3d0ad1db1acf57bc89972471
IlhamTahir <yantaq@bilig.biz>
2021-12-15 18:44:58 +0800

    feat: add cross

--- src/main/java/com/bilitech/yilimusic/YiliMusicApplication.java
+++ src/main/java/com/bilitech/yilimusic/YiliMusicApplication.java
@@ -2,8 +2,10 @@ package com.bilitech.yilimusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class YiliMusicApplication {
 
     public static void main(String[] args) {
