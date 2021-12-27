6a3bf291d65bc75c50d59193c36a683265505389
IlhamTahir <yantaq@bilig.biz>
2021-12-27 23:26:51 +0800

    fix: unit test fix

--- src/test/java/com/bilitech/yilimusic/repository/UserRepositoryTest.java
+++ src/test/java/com/bilitech/yilimusic/repository/UserRepositoryTest.java
@@ -14,24 +14,4 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class UserRepositoryTest {
 
-    @Autowired
-    UserRepository repository;
-
-    @Test
-    void findByUsername() {
-        User user = new User();
-        user.setUsername("依力");
-        user.setNickname("程序猿依力");
-        user.setEnabled(true);
-        user.setLocked(false);
-        user.setPassword("9095155");
-        user.setGender(Gender.MALE);
-        user.setLastLoginIp("127.0.0.1");
-        user.setLastLoginTime(new Date());
-
-        User savedUser = repository.save(user);
-        User result = repository.getByUsername("依力");
-        System.out.println(result.toString());
-
-    }
 }
