62de600453caab440249ff18125294ce25ffac7c
IlhamTahir <yantaq@bilig.biz>
2022-02-07 20:08:15 +0800

    day26

--- src/main/java/com/bilitech/yilimusic/controller/WeixinController.java
+++ src/main/java/com/bilitech/yilimusic/controller/WeixinController.java
@@ -1,13 +1,9 @@
 package com.bilitech.yilimusic.controller;
 
 import me.chanjar.weixin.common.api.WxConsts;
-import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
-import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
-import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,15 +17,13 @@ public class WeixinController {
 
     @GetMapping("/auth_url")
     public String getAuthUrl(@PathParam("redirectUrl") String redirectUrl) {
-        return wxMpService.getOAuth2Service().buildAuthorizationUrl(redirectUrl, WxConsts.OAuth2Scope.SNSAPI_USERINFO, null);
+        return wxMpService.getOAuth2Service().buildAuthorizationUrl(
+                redirectUrl,
+                WxConsts.OAuth2Scope.SNSAPI_USERINFO,
+                null
+        );
     }
 
-
-    @PostMapping("/get_user_info")
-    public WxOAuth2UserInfo getUserInfo(@PathParam("code") String code) throws WxErrorException {
-        WxOAuth2AccessToken wxOAuth2AccessToken = wxMpService.getOAuth2Service().getAccessToken(code);
-        return wxMpService.getOAuth2Service().getUserInfo(wxOAuth2AccessToken, null);
-    }
 
     @Autowired
     public void setWxMpService(WxMpService wxMpService) {
